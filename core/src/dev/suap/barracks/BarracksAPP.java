d778bd82275639a582b53d4e6808021474af7922
suap-dev <suap.dev@gmail.com>
2023-02-06 20:37:02 +0100

    init

---
+++ core/src/dev/suap/barracks/BarracksAPP.java
@@ -0,0 +1,19 @@
+package dev.suap.barracks;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class BarracksAPP extends ApplicationAdapter {	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
