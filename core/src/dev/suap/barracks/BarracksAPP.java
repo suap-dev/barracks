65dd6312a0102f1ca33d65a2510f9570f9988831
suap-dev <suap.dev@gmail.com>
2023-02-10 03:31:23 +0100

    add screen viewport

--- core/src/dev/suap/barracks/BarracksAPP.java
+++ core/src/dev/suap/barracks/BarracksAPP.java
@@ -2,50 +2,45 @@ package dev.suap.barracks;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Interpolation;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-
-import dev.suap.barracks.classes.MyRectangle;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class BarracksAPP extends ApplicationAdapter {
-	ShapeRenderer sRenderer;
-	Rectangle r1;
-	MyRectangle r2;
-	float windowWidth;
-	float windowHeight;
+	SpriteBatch batch;
+	Texture img;
+	ScreenViewport viewport;
+
+	private static final float METER = 16;
 
 	@Override
 	public void create() {
-		windowWidth = Gdx.graphics.getWidth();
-		windowHeight = Gdx.graphics.getHeight();
-		sRenderer = new ShapeRenderer();
-
-		r1 = new Rectangle(10, 10, 200, 100);
-
-		r2 = new MyRectangle(new Vector2(windowWidth / 2, windowHeight / 2), 200, 100);
-		r2.setColor(Color.OLIVE);
-		// r2.setAngularVelocity(-18);
+		viewport = new ScreenViewport();
+		viewport.setUnitsPerPixel(1 / METER);
+		viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render() {
-		// update
-		r2.update(Gdx.graphics.getDeltaTime());
-		// Gdx.app.log("r2", r2.toString());
+		ScreenUtils.clear(Color.OLIVE);
 
-		if (Gdx.input.isKeyPressed(Keys.ENTER))
-			r2.addInterpolatedRotation(90, 1.5f, Interpolation.pow3);
+		batch.setProjectionMatrix(viewport.getCamera().combined);
+		batch.begin();
+		batch.draw(img, -viewport.getWorldWidth() / 2 + 1, -viewport.getWorldHeight() / 2 + 1, img.getWidth() / METER,
+				img.getHeight() / METER);
+		batch.end();
 
-		// draw
-		ScreenUtils.clear(0, 0.07f, 0.1f, 1);
-		sRenderer.begin(ShapeType.Filled);
-		r2.draw(sRenderer);
-		sRenderer.end();
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void resize(int width, int height) {
 	}
 }
